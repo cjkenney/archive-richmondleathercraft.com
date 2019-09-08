e24572b99639bbb2d8e2906089a5860eb21ee3c6
CJ Kenney <cjkenney.dev@gmail.com>
2019-09-08 11:55:54 -0500

    uppercase components

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import './header.scss';
+import './Header.scss';
 
 const Header = ({ siteTitle }) => (
   <header>
