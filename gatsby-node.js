ae6d1df0ad008f4b55d5a35feaf4c63938e9056f
CJ Kenney <cjkenney.dev@gmail.com>
2019-11-03 18:42:14 -0600

    create product pages based on slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,50 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `StripeSku`) {
+    console.log(node.internal.type);
+    console.log(node.id);
+    let id = node.id;
+    console.log(node.children);
+    createNodeField({
+      node,
+      name: `slug`,
+      value: id,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions;
+  const result = await graphql(`
+    query {
+      allStripeSku {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  result.data.allStripeSku.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/product-page.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    });
+  });
+};
